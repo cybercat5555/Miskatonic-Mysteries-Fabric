f71e652c62c177b7c7eef71d770c3ba5c4d1532d
Aelpecyem <aelpecyem@gmail.com>
2020-08-03 21:17:05 +0200

    reorganize files

--- src/main/java/com/miskatonicmysteries/client/render/ResourceHandler.java
+++ src/main/java/com/miskatonicmysteries/client/render/ResourceHandler.java
@@ -23,7 +23,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ResourceHandler {
-    public static final SpriteIdentifier DEFAULT_OCTAGRAM = new SpriteIdentifier(SpriteAtlasTexture.BLOCK_ATLAS_TEX, new Identifier(Constants.MOD_ID, "blocks/octagrams/octagram_generic"));
+    public static final SpriteIdentifier DEFAULT_OCTAGRAM = new SpriteIdentifier(SpriteAtlasTexture.BLOCK_ATLAS_TEX, new Identifier(Constants.MOD_ID, "block/octagram/octagram_generic"));
     public static final Map<Item, SpriteIdentifier> BOOK_SPRITES = new HashMap<>();
     public static final Map<BlockOctagram, SpriteIdentifier> OCTAGRAM_SPRITES = new HashMap<>();
     public static final Map<Identifier, Model> STATUE_MODELS = new HashMap<>();
@@ -35,28 +35,28 @@ public class ResourceHandler {
         });
         ResourceHandler.addBookTextureFor(ModObjects.NECRONOMICON, new Identifier(Constants.MOD_ID, "misc/book_necronomicon"));
 
-        ResourceHandler.addOctagramTextureFor(ModObjects.CTHULHU_OCTAGRAM, new Identifier(Constants.MOD_ID, "blocks/octagrams/cthulhu_octagram"));
-        ResourceHandler.addOctagramTextureFor(ModObjects.HASTUR_OCTAGRAM, new Identifier(Constants.MOD_ID, "blocks/octagrams/hastur_octagram"));
-        ResourceHandler.addOctagramTextureFor(ModObjects.SHUB_OCTAGRAM, new Identifier(Constants.MOD_ID, "blocks/octagrams/shub_octagram"));
+        ResourceHandler.addOctagramTextureFor(ModObjects.CTHULHU_OCTAGRAM, new Identifier(Constants.MOD_ID, "block/octagram/cthulhu_octagram"));
+        ResourceHandler.addOctagramTextureFor(ModObjects.HASTUR_OCTAGRAM, new Identifier(Constants.MOD_ID, "block/octagram/hastur_octagram"));
+        ResourceHandler.addOctagramTextureFor(ModObjects.SHUB_OCTAGRAM, new Identifier(Constants.MOD_ID, "block/octagram/shub_octagram"));
 
         addStatueModelFor(Constants.Affiliation.CTHULHU, new CthulhuStatueModel());
         addStatueModelFor(Constants.Affiliation.HASTUR, new HasturStatueModel());
         addStatueModelFor(Constants.Affiliation.SHUB, new ShubStatueModel());
 
-        addStatueTextureFor(ModObjects.CTHULHU_STATUE_GOLD, new Identifier(Constants.MOD_ID, "blocks/statues/cthulhu_statue_gold"));
-        addStatueTextureFor(ModObjects.CTHULHU_STATUE_MOSSY, new Identifier(Constants.MOD_ID, "blocks/statues/cthulhu_statue_mossy"));
-        addStatueTextureFor(ModObjects.CTHULHU_STATUE_PRISMARINE, new Identifier(Constants.MOD_ID, "blocks/statues/cthulhu_statue_prismarine"));
-        addStatueTextureFor(ModObjects.CTHULHU_STATUE_STONE, new Identifier(Constants.MOD_ID, "blocks/statues/cthulhu_statue_stone"));
+        addStatueTextureFor(ModObjects.CTHULHU_STATUE_GOLD, new Identifier(Constants.MOD_ID, "block/statue/cthulhu_statue_gold"));
+        addStatueTextureFor(ModObjects.CTHULHU_STATUE_MOSSY, new Identifier(Constants.MOD_ID, "block/statue/cthulhu_statue_mossy"));
+        addStatueTextureFor(ModObjects.CTHULHU_STATUE_PRISMARINE, new Identifier(Constants.MOD_ID, "block/statue/cthulhu_statue_prismarine"));
+        addStatueTextureFor(ModObjects.CTHULHU_STATUE_STONE, new Identifier(Constants.MOD_ID, "block/statue/cthulhu_statue_stone"));
 
-        addStatueTextureFor(ModObjects.HASTUR_STATUE_GOLD, new Identifier(Constants.MOD_ID, "blocks/statues/hastur_statue_gold"));
-        addStatueTextureFor(ModObjects.HASTUR_STATUE_MOSSY, new Identifier(Constants.MOD_ID, "blocks/statues/hastur_statue_mossy"));
-        addStatueTextureFor(ModObjects.HASTUR_STATUE_TERRACOTTA, new Identifier(Constants.MOD_ID, "blocks/statues/hastur_statue_terracotta"));
-        addStatueTextureFor(ModObjects.HASTUR_STATUE_STONE, new Identifier(Constants.MOD_ID, "blocks/statues/hastur_statue_stone"));
+        addStatueTextureFor(ModObjects.HASTUR_STATUE_GOLD, new Identifier(Constants.MOD_ID, "block/statue/hastur_statue_gold"));
+        addStatueTextureFor(ModObjects.HASTUR_STATUE_MOSSY, new Identifier(Constants.MOD_ID, "block/statue/hastur_statue_mossy"));
+        addStatueTextureFor(ModObjects.HASTUR_STATUE_TERRACOTTA, new Identifier(Constants.MOD_ID, "block/statue/hastur_statue_terracotta"));
+        addStatueTextureFor(ModObjects.HASTUR_STATUE_STONE, new Identifier(Constants.MOD_ID, "block/statue/hastur_statue_stone"));
 
-        addStatueTextureFor(ModObjects.SHUB_STATUE_GOLD, new Identifier(Constants.MOD_ID, "blocks/statues/shub_statue_gold"));
-        addStatueTextureFor(ModObjects.SHUB_STATUE_MOSSY, new Identifier(Constants.MOD_ID, "blocks/statues/shub_statue_mossy"));
-        addStatueTextureFor(ModObjects.SHUB_STATUE_BLACKSTONE, new Identifier(Constants.MOD_ID, "blocks/statues/shub_statue_blackstone"));
-        addStatueTextureFor(ModObjects.SHUB_STATUE_STONE, new Identifier(Constants.MOD_ID, "blocks/statues/shub_statue_stone"));
+        addStatueTextureFor(ModObjects.SHUB_STATUE_GOLD, new Identifier(Constants.MOD_ID, "block/statue/shub_statue_gold"));
+        addStatueTextureFor(ModObjects.SHUB_STATUE_MOSSY, new Identifier(Constants.MOD_ID, "block/statue/shub_statue_mossy"));
+        addStatueTextureFor(ModObjects.SHUB_STATUE_BLACKSTONE, new Identifier(Constants.MOD_ID, "block/statue/shub_statue_blackstone"));
+        addStatueTextureFor(ModObjects.SHUB_STATUE_STONE, new Identifier(Constants.MOD_ID, "block/statue/shub_statue_stone"));
 
     }
 
