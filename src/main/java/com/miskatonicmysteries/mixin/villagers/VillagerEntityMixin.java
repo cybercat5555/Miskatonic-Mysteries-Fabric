27b26ecbf137535549aeb35fc6de082c3616279e
Aelpecyem <aelpecyem@gmail.com>
2021-01-29 22:17:05 +0100

    buff charming personality

--- src/main/java/com/miskatonicmysteries/mixin/villagers/VillagerEntityMixin.java
+++ src/main/java/com/miskatonicmysteries/mixin/villagers/VillagerEntityMixin.java
@@ -5,6 +5,9 @@ import com.miskatonicmysteries.common.feature.interfaces.Ascendant;
 import com.miskatonicmysteries.common.lib.util.CapabilityUtil;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityInteraction;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.village.VillageGossipType;
 import net.minecraft.village.VillagerGossips;
@@ -28,10 +31,15 @@ public class VillagerEntityMixin {
         Optional<Ascendant> optionalAscendant = Ascendant.of(entity);
         if (optionalAscendant.isPresent() && CapabilityUtil.hasBlessing(optionalAscendant.get(), Blessing.CHARMING_PERSONALITY)) {
             if (interaction == EntityInteraction.ZOMBIE_VILLAGER_CURED) {
-                this.gossip.startGossip(entity.getUuid(), VillageGossipType.MAJOR_POSITIVE, 35);
-                this.gossip.startGossip(entity.getUuid(), VillageGossipType.MINOR_POSITIVE, 40);
+                this.gossip.startGossip(entity.getUuid(), VillageGossipType.MAJOR_POSITIVE, 40);
+                this.gossip.startGossip(entity.getUuid(), VillageGossipType.MINOR_POSITIVE, 60);
             } else if (interaction == EntityInteraction.TRADE) {
                 this.gossip.startGossip(entity.getUuid(), VillageGossipType.TRADING, 6);
+                if (entity instanceof LivingEntity) {
+                    ((LivingEntity) entity).addStatusEffect(new StatusEffectInstance(StatusEffects.SATURATION, 2400, 0, true, false, false));
+                    ((LivingEntity) entity).addStatusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 2400, 0, true, false, false));
+                    ((LivingEntity) entity).heal(5F);
+                }
             }
             //completely ignore bad interactions
             ci.cancel();
