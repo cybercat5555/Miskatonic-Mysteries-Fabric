a33254493c06a945aecbb145d933f96265c0490b
Aelpecyem <aelpecyem@gmail.com>
2021-03-06 23:59:23 +0100

    holy shit moriya i did not know there was a copyfrom callback wtf

--- src/main/java/com/miskatonicmysteries/common/MiskatonicMysteries.java
+++ src/main/java/com/miskatonicmysteries/common/MiskatonicMysteries.java
@@ -1,6 +1,10 @@
 package com.miskatonicmysteries.common;
 
 import com.miskatonicmysteries.api.MiskatonicMysteriesAPI;
+import com.miskatonicmysteries.api.interfaces.Ascendant;
+import com.miskatonicmysteries.api.interfaces.MalleableAffiliated;
+import com.miskatonicmysteries.api.interfaces.Sanity;
+import com.miskatonicmysteries.api.interfaces.SpellCaster;
 import com.miskatonicmysteries.common.feature.ModCommand;
 import com.miskatonicmysteries.common.handler.networking.packet.SpellPacket;
 import com.miskatonicmysteries.common.handler.networking.packet.SyncSpellCasterDataPacket;
@@ -12,6 +16,7 @@ import dev.emi.trinkets.api.TrinketSlots;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.command.argument.ArgumentTypes;
 import net.minecraft.command.argument.IdentifierArgumentType;
@@ -53,5 +58,32 @@ public class MiskatonicMysteries implements ModInitializer {
         ServerPlayNetworking.registerGlobalReceiver(InvokeManiaPacket.ID, InvokeManiaPacket::handle);
         ServerPlayNetworking.registerGlobalReceiver(SyncSpellCasterDataPacket.ID, SyncSpellCasterDataPacket::handleFromClient);
         ServerPlayNetworking.registerGlobalReceiver(SpellPacket.ID, SpellPacket::handleFromClient);
+
+        ServerPlayerEvents.COPY_FROM.register((player, oldPlayer, isDead) -> {
+            Sanity.of(oldPlayer).ifPresent(oldSanity -> Sanity.of(player).ifPresent(sanity -> {
+                sanity.setSanity(oldSanity.getSanity(), true);
+                sanity.getSanityCapExpansions().putAll(oldSanity.getSanityCapExpansions());
+                sanity.syncSanityData();
+            }));
+
+            SpellCaster.of(oldPlayer).ifPresent(oldCaster -> SpellCaster.of(player).ifPresent(caster -> {
+                caster.setMaxSpells(oldCaster.getMaxSpells());
+                caster.setPowerPool(oldCaster.getPowerPool());
+                caster.getLearnedEffects().addAll(oldCaster.getLearnedEffects());
+                caster.getLearnedMediums().addAll(oldCaster.getLearnedMediums());
+                caster.getSpells().addAll(oldCaster.getSpells());
+                caster.syncSpellData();
+            }));
+
+            Ascendant.of(oldPlayer).ifPresent(oldAscendant -> Ascendant.of(player).ifPresent(ascendant -> {
+                ascendant.setAscensionStage(oldAscendant.getAscensionStage());
+                ascendant.getBlessings().addAll(oldAscendant.getBlessings());
+            }));
+
+            MalleableAffiliated.of(oldPlayer).ifPresent(oldAffiliation -> MalleableAffiliated.of(player).ifPresent(affiliation -> {
+                affiliation.setAffiliation(oldAffiliation.getAffiliation(false), false);
+                affiliation.setAffiliation(oldAffiliation.getAffiliation(true), true);
+            }));
+        });
     }
 }
