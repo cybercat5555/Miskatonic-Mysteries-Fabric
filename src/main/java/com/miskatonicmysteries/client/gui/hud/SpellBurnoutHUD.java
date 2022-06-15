6c1bc2439c24fbcdaf3793a0df416803ac1f4725
Aelpecyem <aelpecyem@gmail.com>
2022-06-15 14:58:21 +0200

    fix burnout HUD

--- src/main/java/com/miskatonicmysteries/client/gui/hud/SpellBurnoutHUD.java
+++ src/main/java/com/miskatonicmysteries/client/gui/hud/SpellBurnoutHUD.java
@@ -9,12 +9,15 @@ import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.render.BufferBuilder;
+import net.minecraft.client.render.BufferRenderer;
+import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.Tessellator;
 import net.minecraft.client.render.VertexFormat;
 import net.minecraft.client.render.VertexFormats;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Matrix4f;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 
@@ -26,24 +29,42 @@ public class SpellBurnoutHUD extends DrawableHelper {
 	public void render(MinecraftClient client, int scaledWidth, int scaledHeight, PlayerEntity player) {
 		SpellCaster.of(player).ifPresent(caster -> {
 			if (caster.getSpellCooldown() > 0) {
+				RenderSystem.setShader(GameRenderer::getPositionTexShader);
 				float burnout = MathHelper.clamp(caster.getSpellCooldown() / 80F, 0, 0.5F);
+				RenderSystem.enableTexture();
+				RenderSystem.setShaderTexture(0, VIGNETTE_TEXTURE);
+				BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
+				RenderSystem.enableBlend();
+				RenderSystem.defaultBlendFunc();
+				RenderSystem.setShaderColor(1F, 1F, 1F, burnout);
+				bufferBuilder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE);
+				bufferBuilder.vertex(0.0D, scaledHeight, -90.0D).texture(0.0F, 1.0F).color(1F, 1F, 1F, burnout).next();
+				bufferBuilder.vertex(scaledWidth, scaledHeight, -90.0D).texture(1.0F, 1.0F).color(1F, 1F, 1F, burnout).next();
+				bufferBuilder.vertex(scaledWidth, 0.0D, -90.0D).texture(1.0F, 0.0F).color(1F, 1F, 1F, burnout).next();
+				bufferBuilder.vertex(0.0D, 0.0D, -90.0D).texture(0.0F, 0.0F).color(1F, 1F, 1F, burnout).next();
+				bufferBuilder.end();
+				BufferRenderer.draw(bufferBuilder);
+				RenderSystem.disableBlend();
+			/*
 				RenderSystem.enableBlend();
 				RenderSystem.defaultBlendFunc();
 				RenderSystem.disableDepthTest();
 				RenderSystem.depthMask(false);
-				RenderSystem.setShaderColor(1F, 1F, 1F, burnout);
-				RenderSystem.setShaderTexture(0, VIGNETTE_TEXTURE);
+				RenderSystem.enableTexture();
 				Tessellator tessellator = Tessellator.getInstance();
 				BufferBuilder bufferBuilder = tessellator.getBuffer();
-				bufferBuilder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE);
-				bufferBuilder.vertex(0.0D, scaledHeight, -90.0D).texture(0.0F, 1.0F).next();
-				bufferBuilder.vertex(scaledWidth, scaledHeight, -90.0D).texture(1.0F, 1.0F).next();
-				bufferBuilder.vertex(scaledWidth, 0.0D, -90.0D).texture(1.0F, 0.0F).next();
-				bufferBuilder.vertex(0.0D, 0.0D, -90.0D).texture(0.0F, 0.0F).next();
+				RenderSystem.setShaderColor(1, 1, 1, burnout);
+				bufferBuilder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE_COLOR);
+				RenderSystem.setShaderTexture(0, VIGNETTE_TEXTURE);
+				bufferBuilder.vertex(0.0D, scaledHeight, -90.0D).texture(0.0F, 1.0F).color(1F, 1F, 1F, burnout).next();
+				bufferBuilder.vertex(scaledWidth, scaledHeight, -90.0D).texture(1.0F, 1.0F).color(1F, 1F, 1F, burnout).next();
+				bufferBuilder.vertex(scaledWidth, 0.0D, -90.0D).texture(1.0F, 0.0F).color(1F, 1F, 1F, burnout).next();
+				bufferBuilder.vertex(0.0D, 0.0D, -90.0D).texture(0.0F, 0.0F).color(1F, 1F, 1F, burnout).next();
 				tessellator.draw();
 				RenderSystem.depthMask(true);
 				RenderSystem.enableDepthTest();
-				RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+				RenderSystem.disableTexture();
+				RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);*/
 			}
 		});
 	}
