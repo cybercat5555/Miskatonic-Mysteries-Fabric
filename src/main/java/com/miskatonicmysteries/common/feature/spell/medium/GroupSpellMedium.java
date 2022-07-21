5546dcfabbe7fef7b9cf5f40f95cb2a3d4b4ffac
Aelpecyem <aelpecyem@gmail.com>
2022-07-21 13:57:30 +0200

    fix Group Spell Medium

--- src/main/java/com/miskatonicmysteries/common/feature/spell/medium/GroupSpellMedium.java
+++ src/main/java/com/miskatonicmysteries/common/feature/spell/medium/GroupSpellMedium.java
@@ -1,7 +1,10 @@
 package com.miskatonicmysteries.common.feature.spell.medium;
 
+import com.miskatonicmysteries.api.MiskatonicMysteriesAPI;
 import com.miskatonicmysteries.api.registry.SpellEffect;
 import com.miskatonicmysteries.api.registry.SpellMedium;
+import com.miskatonicmysteries.common.feature.entity.HarrowEntity;
+import com.miskatonicmysteries.common.feature.entity.TentacleEntity;
 import com.miskatonicmysteries.common.util.Constants;
 
 import net.minecraft.entity.Entity;
@@ -22,13 +25,26 @@ public class GroupSpellMedium extends SpellMedium {
 		boolean successfulCast = false;
 		List<Entity> entities = world
 			.getOtherEntities(null, caster.getBoundingBox().expand(3 + (4 * intensity), 3 + (4 * intensity), 3 + (4 * intensity)),
-							  entity -> entity instanceof LivingEntity);
+							  entity -> isValidTarget(caster, entity));
 		for (Entity otherEntity : entities) {
 			if (effect.effect(world, caster, otherEntity, otherEntity.getPos(), this, intensity, caster)) {
 				successfulCast = true;
 			}
 		}
 		return !caster.isDead() && successfulCast;
+	}
+
+	private boolean isValidTarget(LivingEntity caster, Entity entity) {
+		if (entity instanceof LivingEntity) {
+			if (entity == caster.getAttacker() || entity instanceof TentacleEntity || entity instanceof HarrowEntity) {
+				return false;
+			}
+			if (MiskatonicMysteriesAPI.isDefiniteAffiliated(caster)) {
+				return MiskatonicMysteriesAPI.getNonNullAffiliation(caster, false) == MiskatonicMysteriesAPI.getNonNullAffiliation(entity, false);
+			}
+			return true;
+		}
+		return false;
 	}
 
 	@Override
