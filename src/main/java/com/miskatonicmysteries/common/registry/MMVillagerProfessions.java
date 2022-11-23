74df7c68426f11a6b3e12fa7cca72f34bad31dfe
mrsterner <simon@sterner.dev>
2022-11-23 23:13:11 +0100

    tihi

--- src/main/java/com/miskatonicmysteries/common/registry/MMVillagerProfessions.java
+++ src/main/java/com/miskatonicmysteries/common/registry/MMVillagerProfessions.java
@@ -9,6 +9,7 @@ import net.fabricmc.fabric.api.object.builder.v1.villager.VillagerProfessionBuil
 import net.fabricmc.fabric.api.object.builder.v1.world.poi.PointOfInterestHelper;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -22,7 +23,7 @@ public class MMVillagerProfessions {
     public static final RegistryKey<PointOfInterestType> HASTUR = of("hastur");
     public static final PointOfInterestType PSYCHO_POI = registerPOI("psycho_poi", MMObjects.CHEMISTRY_SET);
     public static final VillagerProfession PSYCHONAUT = registerProfession("psychonaut",
-            RegistryKey.of(Registry.POINT_OF_INTEREST_TYPE_KEY, new Identifier(Constants.MOD_ID, "psycho_poi")));
+            RegistryKey.of(Registry.POINT_OF_INTEREST_TYPE_KEY, new Identifier(Constants.MOD_ID, "psycho_poi")), SoundEvents.BLOCK_BREWING_STAND_BREW);
 
     public static void init() {
         MiskatonicMysteries.LOGGER.debug("Registering Villagers for " + Constants.MOD_ID);
@@ -49,9 +50,9 @@ public class MMVillagerProfessions {
                 1,1, ImmutableSet.copyOf(block.getStateManager().getStates()));
     }
 
-    public static VillagerProfession registerProfession(String name, RegistryKey<PointOfInterestType> type){
+    public static VillagerProfession registerProfession(String name, RegistryKey<PointOfInterestType> type, SoundEvent sound){
         return Registry.register(Registry.VILLAGER_PROFESSION, new Identifier(Constants.MOD_ID,name),
                 VillagerProfessionBuilder.create().id(new Identifier(Constants.MOD_ID,name)).workstation(type)
-                        .workSound(SoundEvents.ENTITY_VILLAGER_WORK_ARMORER).build());
+                        .workSound(sound).build());
     }
 }
